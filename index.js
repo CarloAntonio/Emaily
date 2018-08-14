879b9bb01d6d2d27d364d83cd9321d5e755b8c95
Carlo Bilbao <cbilbao88@gmail.com>
2018-08-13 21:29:41 -0700

    changed greeting

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
+// Dynamic Port Binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
